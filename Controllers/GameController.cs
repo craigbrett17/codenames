820fe6d214254b3ae04aa6064a887979f607d2cf
Craig Brett <craigbrett17@aol.com>
2020-05-23 14:45:02 +0100

    Initial work on a games controller and made some initial views

---
+++ Controllers/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using accessible_codenames.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace accessible_codenames.Controllers
+{
+    public class GameController : Controller
+    {
+        private IGameService _gameService;
+        private ILogger<GameController> _logger;
+
+        public GameController(IGameService gameService, ILogger<GameController> logger)
+        {
+            _gameService = gameService;
+            _logger = logger;
+        }
+
+        public IActionResult Index(string id)
+        {
+            return View();
+        }
+
+        public IActionResult Create(string id)
+        {
+            var game = _gameService.CreateGame(wordList: id);
+            _logger.LogInformation($"Game created. With ID: {game.Id}. Returning redirect");
+            return RedirectToAction("Index", "Game", new { id = game.Id.ToString() });
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,12 +22,7 @@ namespace accessible_codenames.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
+    
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
