f7c97068f348b6e4336b26780e7f41bfba781127
Craig Brett <craigbrett17@aol.com>
2020-05-25 17:29:47 +0100

    Add an about page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,11 @@ namespace accessible_codenames.Controllers
         {
             return View();
         }
+
+        public IActionResult About()
+        {
+            return View();
+        }
     
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
